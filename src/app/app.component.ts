1a4fbe49f8024e6d5862b15dfa6dc59e71caf029
unknown <georgealromhin@gmail>
2020-12-01 12:30:08 +0300

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { APP_NAME } from './config/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Privacy & Terms |';
+
+  constructor(
+    private title:Title
+  ){
+    title.setTitle(`Privacy & Terms | ${APP_NAME}`);
+  }
+
+  
 }
