27380eddb714fa053a797082fce9ae013bebf52f
unknown <georgealromhin@gmail>
2020-12-01 08:47:29 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TabComponent } from './components/tab/tab.component';
+import { OverviewComponent } from './components/tab-items/overview/overview.component';
+import { PrivacyPolicyComponent } from './components/tab-items/privacy-policy/privacy-policy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    TabComponent,
+    OverviewComponent,
+    PrivacyPolicyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
